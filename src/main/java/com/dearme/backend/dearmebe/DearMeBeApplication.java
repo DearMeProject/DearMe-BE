8fa73264407512b5a770732cfd509bcd626025b2
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-11-08 15:48:15 +0900

    chore(init): 프로젝트 초기 설정

---
+++ src/main/java/com/dearme/backend/dearmebe/DearMeBeApplication.java
@@ -0,0 +1,13 @@
+package com.dearme.backend.dearmebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearMeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DearMeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dearme/backend/dearmebe/DearMeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dearme.backend.dearmebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearMeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
