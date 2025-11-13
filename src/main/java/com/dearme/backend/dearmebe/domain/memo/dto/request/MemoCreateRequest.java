4dc565d025b6b6360f7febcbb3f48e5d097d0ce4
김은서 <kimeunseo@gim-eunseoui-MacBookAir.local>
2025-11-13 09:23:58 +0900

    refactor(memo): 불필요한 주석 삭제

--- src/main/java/com/dearme/backend/dearmebe/domain/memo/dto/request/MemoCreateRequest.java
+++ src/main/java/com/dearme/backend/dearmebe/domain/memo/dto/request/MemoCreateRequest.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class MemoCreateRequest {
 
-    // date: YYYY-MM-DD 형식 검증 -> 프론트단 아닌가?
+    // date: YYYY-MM-DD 형식 검증
     @NotBlank(message = "날짜는 필수 입력 값입니다.")
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜 형식이 올바르지 않습니다. (YYYY-MM-DD)")
     private String date;
