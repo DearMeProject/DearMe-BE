867affa6230dfb786f68ad04af22d148d322aa90
김은서 <kimeunseo@gim-eunseoui-MacBookAir.local>
2025-11-13 14:38:23 +0900

    feat(memo): 메모 상세 조회 API 엔드포인트 추가

--- src/main/java/com/dearme/backend/dearmebe/domain/memo/controller/MemoController.java
+++ src/main/java/com/dearme/backend/dearmebe/domain/memo/controller/MemoController.java
@@ -2,7 +2,9 @@ package com.dearme.backend.dearmebe.domain.memo.controller;
 
 import com.dearme.backend.dearmebe.domain.memo.dto.request.MemoCreateRequest;
 import com.dearme.backend.dearmebe.domain.memo.dto.response.MemoCreateResponse;
+import com.dearme.backend.dearmebe.domain.memo.dto.response.MemoDetailResponse;
 import com.dearme.backend.dearmebe.domain.memo.dto.response.MemoListResponse;
+import com.dearme.backend.dearmebe.domain.memo.entity.Memo;
 import com.dearme.backend.dearmebe.domain.memo.service.MemoService;
 import com.dearme.backend.dearmebe.global.response.ApiResponse;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api/memos")
 public class MemoController {
 
     private final MemoService memoService;
@@ -19,7 +22,7 @@ public class MemoController {
         this.memoService = memoService;
     }
 
-    @PostMapping("/api/memos")
+    @PostMapping
     public ResponseEntity<ApiResponse<MemoCreateResponse>>  createMemo(
             @RequestHeader("X-Client-Id") String ClientId,
             @RequestBody @Valid MemoCreateRequest request
@@ -29,12 +32,22 @@ public class MemoController {
                 .body(ApiResponse.created("메모 작성 완료", memoResponse));
     }
 
-    @GetMapping("/api/memos")
+    @GetMapping
     public ResponseEntity<ApiResponse<MemoListResponse>> getAllMemos(
             @RequestHeader("X-Client-Id") String clientId
     ) {
         MemoListResponse response = memoService.getAllMemos(clientId);
         return ResponseEntity
                 .ok(ApiResponse.ok("조회 성공", response));
+    }
+
+    @GetMapping("/{memoId}")
+    public ResponseEntity<ApiResponse<MemoDetailResponse>> getMemoDetail(
+        @RequestHeader("X-Client-Id") String clientId,
+        @PathVariable Long memoId
+    ) {
+        MemoDetailResponse response = memoService.getMemoDetail(clientId, memoId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.ok("조회 성공", response));
     }
 }
