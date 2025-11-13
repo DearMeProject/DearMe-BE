df943bc8a1e1285ca01c72a0336f6f4f7f77ea6c
김은서 <kimeunseo@gim-eunseoui-MacBookAir.local>
2025-11-13 14:42:00 +0900

    feat(memo): 메모 단건 조회용 findById 메서드 추가

--- src/main/java/com/dearme/backend/dearmebe/domain/memo/repository/MemoRepository.java
+++ src/main/java/com/dearme/backend/dearmebe/domain/memo/repository/MemoRepository.java
@@ -4,10 +4,13 @@ import com.dearme.backend.dearmebe.domain.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
     List<Memo> findByClientId(String clientId);
     List<Memo> findAllByClientIdOrderByDateAsc(String clientId);
+
+    Optional<Memo> findById(Long id);
 }
 
