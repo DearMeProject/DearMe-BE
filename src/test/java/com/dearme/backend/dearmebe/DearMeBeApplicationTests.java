826ad1756e417a7276696946b7c041cd2f851818
김은서 <kimeunseo@gim-eunseoui-MacBookAir.local>
2025-11-14 15:46:31 +0900

    chore(test): DearMeBeApplicationTests에 test 프로필 활성화 설정 추가

--- src/test/java/com/dearme/backend/dearmebe/DearMeBeApplicationTests.java
+++ src/test/java/com/dearme/backend/dearmebe/DearMeBeApplicationTests.java
@@ -2,8 +2,10 @@ package com.dearme.backend.dearmebe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DearMeBeApplicationTests {
 
     @Test
